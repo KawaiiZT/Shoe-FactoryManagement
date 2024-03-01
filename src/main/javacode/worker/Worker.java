bba3554177dfa2e63e55b2bc8d365411e210c2c4
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2024-03-01 20:45:34 +0700

    tabledatademo

--- src/main/javacode/worker/Worker.java
+++ src/main/javacode/worker/Worker.java
@@ -16,7 +16,14 @@ public class Worker{
     private JFrame fr;
     private JTable tableinfo;
     public Worker(){
+        String data[][]={ {"101","Trisit","Charoenparipat","087XXXXX","MALE","7/8/2547","000000","1101700361xxx"},    
+                          };
+    String column[]={"ID","FIRSTNAME","LASTNAME","PHONENUMBER,SEX,DAY/MONTH/YEAR,PASSPORT,CITIZENCARD"};        
         fr = new JFrame("ShoeFactoryManagement");
+        tableinfo = new JTable(data, column);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.pack();
+        fr.setVisible(true);
     }
     
 }
