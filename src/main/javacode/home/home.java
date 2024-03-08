d611b3c12f2e50b7d355398c4c5ee8c99f5f5d11
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2024-03-08 20:29:17 +0700

    make an update for home page

--- src/main/javacode/home/home.java
+++ src/main/javacode/home/home.java
@@ -9,6 +9,10 @@ import java.awt.event.*;
 public class home {
     private JFrame fr;
     private JPanel pn1;
+    private JPanel pn2;
+    private JPanel pn3;
+    private JButton itembutton;
+    private JLabel headline;
     private JLabel item;
     private JLabel numofItem;
     private JLabel worker;
@@ -18,12 +22,27 @@ public class home {
     public home(){
         fr = new JFrame("ShoeFactoryManagement");
         pn1 = new JPanel();
+        pn2 = new JPanel();
+        pn3 = new JPanel();
+        headline = new JLabel("Dashboard");
+        itembutton = new JButton();
+        item = new JLabel("Item in Stock");
         fr.setLayout(new BorderLayout());
         MenuDashboard menu = new MenuDashboard();
 
 
         pn1.add(menu);
         fr.add(pn1, BorderLayout.WEST);
+        fr.add(pn2, BorderLayout.CENTER);
+        pn2.setLayout(new GridLayout(10,0));
+        pn2.add(headline);
+        pn2.add(pn3);
+        pn3.setLayout(new GridLayout(1,3,0,20));
+        pn3.add(itembutton);
+        itembutton.setLayout(new GridLayout(4,0));
+        itembutton.add(item);
+        itembutton.setBackground(Color.black);
+
 
 
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
