616a052a5988ac85b99800c25841dfa572ddc15c
EXCompleteWorld <66070258@kmitl.ac.th>
2024-03-25 02:39:12 +0700

    Insert into sql

--- src/main/javacode/worker/DataWorker.java
+++ src/main/javacode/worker/DataWorker.java
@@ -7,15 +7,19 @@ public class DataWorker {
         String url = "jdbc:mysql://localhost:3306/mydb";
         String user = "root";
         String password = "1234";
+        // create
         Connection connection = null;
         Statement s = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url,user,password);
+            connection = DriverManager.getConnection(url, user, password);
             // test
-            String sql = "SELECT * From mydb.worker";
+            /*String sql1 = "Insert Into mydb.worker(firstname, lastname, email, phonenumber, day, month, year, sex, occupation, citizen_id) " +
+                    "VALUES('Kull', 'Su', 'kull', '064', '25', '3', '2024', 'Female', 'null', '113')";*/
+            String sql2 = "SELECT * FROM mydb.worker";
             s = connection.createStatement();
-            ResultSet rec = s.executeQuery(sql);
+            //s.executeUpdate(sql1);
+            ResultSet rec = s.executeQuery(sql2);
             // check collum
             while ((rec != null) && (rec.next())) {
                 // check roll
@@ -23,7 +27,9 @@ public class DataWorker {
                 System.out.println(rec.getString("lastname"));
                 System.out.println(rec.getString("email"));
                 System.out.println(rec.getString("phonenumber"));
-                System.out.println(rec.getDate("date"));
+                System.out.println(rec.getString("day"));
+                System.out.println(rec.getString("month"));
+                System.out.println(rec.getString("year"));
                 System.out.println(rec.getString("sex"));
                 System.out.println(rec.getString("occupation"));
                 System.out.println(rec.getString("citizen_id"));
